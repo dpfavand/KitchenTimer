9c0d2305369d755c7d73ccb33fba02de403d06d7
Daniel Favand <daniel@favand.net>
2026-04-06 21:27:32 -0230

    install adafruit libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define DISPLAY_ADDRESS 0x78 // alternative: 0x7A
+#define BUTTON_PIN D5 //D5
 
 // put function declarations here:
 int myFunction(int, int);
